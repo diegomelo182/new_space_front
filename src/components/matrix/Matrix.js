4b370d6abd3a44705ce35d70c32741cc0ce80594
Diego Melo <diegomelo182@gmail.com>
2020-09-16 11:42:37 -0300

    Commit inicial

---
+++ src/components/matrix/Matrix.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+const Matrix = ({ airportsPosition = [], cloudPosition = [] }) => {
+  return(
+    <div className="matrix">
+      <div className="row">
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+        <div className="col">
+          col
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Matrix;
